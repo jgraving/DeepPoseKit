d383df53574b5899f0fc125ddb1e2dff74d5f76b
Jake <jgraving@gmail.com>
2019-09-19 11:59:37 +0200

    Deprecate UpSampling2D
    tf.keras.layers.UpSampling2D supercedes this

--- deepposekit/models/layers/convolutional.py
+++ deepposekit/models/layers/convolutional.py
@@ -18,6 +18,8 @@ limitations under the License.
 from tensorflow.python.keras.engine import Layer
 from tensorflow.python.keras.engine import InputSpec
 
+from tensorflow.keras.layers import UpSampling2D
+
 from ..backend import resize_images, find_maxima, depth_to_space, space_to_depth
 
 from tensorflow.python.keras.utils.conv_utils import (
@@ -25,83 +27,7 @@ from tensorflow.python.keras.utils.conv_utils import (
     normalize_tuple,
 )
 
-
 __all__ = ["UpSampling2D", "Maxima2D", "SubPixelUpscaling", "SubPixelDownscaling"]
-
-
-class UpSampling2D(Layer):
-    """Upsampling layer for 2D inputs.
-    Repeats the rows and columns of the data
-    by size[0] and size[1] respectively with interpolation.
-    # Arguments
-        size: int, or tuple of 2 integers.
-            The upsampling factors for rows and columns.
-        data_format: A string,
-            one of `channels_last` (default) or `channels_first`.
-            The ordering of the dimensions in the inputs.
-            `channels_last` corresponds to inputs with shape
-            `(batch, height, width, channels)` while `channels_first`
-            corresponds to inputs with shape
-            `(batch, channels, height, width)`.
-            It defaults to the `image_data_format` value found in your
-            Keras config file at `~/.keras/keras.json`.
-            If you never set it, then it will be "channels_last".
-        interpolation: A string,
-            one of 'nearest' (default), 'bilinear', or 'bicubic'
-    # Input shape
-        4D tensor with shape:
-        - If `data_format` is `"channels_last"`:
-            `(batch, rows, cols, channels)`
-        - If `data_format` is `"channels_first"`:
-            `(batch, channels, rows, cols)`
-    # Output shape
-        4D tensor with shape:
-        - If `data_format` is `"channels_last"`:
-            `(batch, upsampled_rows, upsampled_cols, channels)`
-        - If `data_format` is `"channels_first"`:
-            `(batch, channels, upsampled_rows, upsampled_cols)`
-    """
-
-    def __init__(
-        self, size=(2, 2), data_format=None, interpolation="nearest", **kwargs
-    ):
-        super(UpSampling2D, self).__init__(**kwargs)
-        self.data_format = normalize_data_format(data_format)
-        self.interpolation = interpolation
-        self.size = normalize_tuple(size, 2, "size")
-        self.input_spec = InputSpec(ndim=4)
-
-    def compute_output_shape(self, input_shape):
-        if self.data_format == "channels_first":
-            height = (
-                self.size[0] * input_shape[2] if input_shape[2] is not None else None
-            )
-            width = (
-                self.size[1] * input_shape[3] if input_shape[3] is not None else None
-            )
-            return (input_shape[0], input_shape[1], height, width)
-        elif self.data_format == "channels_last":
-            height = (
-                self.size[0] * input_shape[1] if input_shape[1] is not None else None
-            )
-            width = (
-                self.size[1] * input_shape[2] if input_shape[2] is not None else None
-            )
-            return (input_shape[0], height, width, input_shape[3])
-
-    def call(self, inputs):
-        return resize_images(
-            inputs, self.size[0], self.size[1], self.interpolation, self.data_format
-        )
-
-    def get_config(self):
-        config = {
-            "size": self.size,
-            "data_format": self.data_format,
-            "interpolation": self.interpolation,
-        }
-        base_config = super(UpSampling2D, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
 
 
 class Maxima2D(Layer):
