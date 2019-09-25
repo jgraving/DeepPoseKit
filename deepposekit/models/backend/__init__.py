f10fe35d11c6c7de0c7ea6ad5353736b49952810
Jake <jgraving@gmail.com>
2019-09-25 22:39:37 +0200

    Fix imports

--- deepposekit/models/backend/__init__.py
+++ deepposekit/models/backend/__init__.py
@@ -1,5 +1,1 @@
 from __future__ import absolute_import
-
-import deepposekit.models.backend.backend as backend
-import deepposekit.models.backend.utils as utils
-import deepposekit.models.backend.registration as registration
