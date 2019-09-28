f5ce6ff1ccc04d6543470e51d014a4dc78183acd
Jake <jgraving@gmail.com>
2019-09-28 14:33:29 +0200

    Switch to 'deepposekit.annotate'

--- deepposekit/__init__.py
+++ deepposekit/__init__.py
@@ -5,9 +5,9 @@ import warnings
 from deepposekit.io import TrainingGenerator, DataGenerator
 from deepposekit.augment.FlipAxis import FlipAxis
 
-from deepposekit.annotation.gui.Annotator import Annotator
-from deepposekit.annotation.gui.Skeleton import Skeleton
-from deepposekit.annotation.KMeansSampler import KMeansSampler
+from deepposekit.annotate.gui.Annotator import Annotator
+from deepposekit.annotate.gui.Skeleton import Skeleton
+from deepposekit.annotate.KMeansSampler import KMeansSampler
 
 from deepposekit.io.video import VideoReader, VideoWriter
 
