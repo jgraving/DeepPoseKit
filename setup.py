07bd481b9e81204716de1ae39f70d2959c3c8334
Jake <jgraving@gmail.com>
2019-10-09 16:51:38 +0200

    Bump to v0.3.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ MAINTAINER_EMAIL = "jgraving@gmail.com"
 URL = "https://github.com/jgraving/deepposekit"
 LICENSE = "Apache 2.0"
 DOWNLOAD_URL = "https://github.com/jgraving/deepposekit.git"
-VERSION = "0.3.3.dev"
+VERSION = "0.3.4.dev"
 
 
 if __name__ == "__main__":
